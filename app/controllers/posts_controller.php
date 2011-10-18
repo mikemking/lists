27f7df7d681cab59080baba17030e799a34cc043
Mike King <mking@webcodefocus.com>
2011-10-17 21:47:58 -0400

    Finished add, edit, delete of Posts.

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -1,8 +1,9 @@
 <?php
 class PostsController extends AppController
 {
-    var $helpers = array ('Html','Form');
     var $name = 'Posts';
+    var $helpers = array ('Html','Form', 'Session');
+    var $components = array('Session');
 
     function index()
     {
@@ -14,6 +15,46 @@ class PostsController extends AppController
         $this->Post->id = $id;
         $this->set('post', $this->Post->read());
     }
+
+    function add()
+    {
+        if (! empty($this->data))
+        {
+            if ($this->Post->save($this->data)) 
+            {
+                $this->Session->setFlash('Your post has been saved.');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
+
+    function delete($id)
+    {
+        if ($this->Post->delete($id))
+        {
+            $this->Session->setFlash('The post with id: ' . $id . ' has been deleted.');
+            $this->redirect(array('action' => 'index'));
+        }
+    }
+
+    function edit($id = null)
+    {
+        $this->Post->id = $id;
+        if (empty($this->data)) 
+        {
+            $this->data = $this->Post->read();
+        }
+        else
+        {
+            if ($this->Post->save($this->data)) 
+            {
+                $this->Session->setFlash('Your post has been updated.');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
+
+
 
 }
 
--- app/views/posts/index.ctp
+++ app/views/posts/index.ctp
@@ -1,8 +1,10 @@
 <h1>Blog posts</h1>
+<?php echo $this->Html->link('Add Post', array('controller' => 'posts', 'action' => 'add')); ?>
 <table>
     <tr>
         <th>Id</th>
         <th>Title</th>
+        <th>Actions</th>
         <th>Created</th>
     </tr>
     <?php foreach ($posts as $post): ?>
@@ -10,6 +12,10 @@
         <td><?php echo $post['Post']['id']; ?></td>
         <td>
             <?php echo $this->Html->link($post['Post']['title'], array('controller' => 'posts', 'action' => 'view', $post['Post']['id'])); ?>
+        </td>
+        <td>
+            <?php echo $this->Html->link('Edit', array('action' => 'edit', $post['Post']['id']));?>
+            <?php echo $this->Html->link('Delete', array('action' => 'delete', $post['Post']['id']), null, 'Are you sure?')?>
         </td>
         <td><?php echo $post['Post']['created']; ?></td>
     </tr>
